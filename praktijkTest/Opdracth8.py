17b777a9a80cfeb7b5ea22ec4c7b712768e48a89
abdallawi <abdalmounaim@gmail.com>
2019-11-15 15:40:00 +0100

    Add exceptions Opdracht8.py

--- praktijkTest/Opdracth8.py
+++ praktijkTest/Opdracth8.py
@@ -1,22 +1,38 @@
 class BankAccount:
     balance = 0.0
 
+# Constructor raise an exception if the name of the owner is not a string of min 3 characters
     def __init__(self, owner):
-        self.owner = owner
+        if not isinstance(owner, str) or len(owner) < 3:
+            raise ValueError('The owner name must be a string of min 3 letters')
+        else:
+            self.owner = owner.title()
 
     def deposit(self, amount):
         self.balance += amount
         return self.balance
 
+# Function that withdraws the requested amount and returns the new balance
+# Raise an exception if the asked amount is bigger than the balance
     def withdraw(self, amount):
-        self.balance -= amount
+        if amount > self.balance:
+            raise ValueError(f'The amount demanded must be inferior than your balance :  {self.balance}')
+        else:
+            self.balance -= amount
         return self.balance
 
 
-account = BankAccount("Michel")
-print(account.owner)
-print(account.balance)
+try:
+    account = BankAccount("sue")
+except ValueError as ve:
+    print(ve)
+
+print(f'The owner of this account is : {account.owner}')
 account.deposit(235.66)
-print(f' Your current balance is : {account.balance}')
-account.withdraw(20)
-print(f' Your current balance is : {account.balance}')
+
+print(f'Your current balance of {account.owner} is : {account.balance}')
+try:
+    account.withdraw(256)
+except ValueError as ve:
+    print(ve)
+print(f'Your current balance of {account.owner} is :{account.balance}')
